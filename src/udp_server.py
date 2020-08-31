5ac5b783ed14826be99b834f75f254240182f068
Tiberiu Danciu <tiberiud99@gmail.com>
2020-08-31 10:27:35 +0300

    Fixed settings in udp_server.py.
    Signed-off-by: Tiberiu Danciu <tiberiud99@gmail.com>

--- src/udp_server.py
+++ src/udp_server.py
@@ -1,14 +1,13 @@
 from pyais.stream import UDPStream
 from src.duplicate_checker import DuplicateChecker
 import logging
+import settings
 
 
 def udp_server(sync_q):
-    host = "0.0.0.0"
-    port = 10110
     checker = DuplicateChecker()
 
-    for msg in UDPStream(host, port):
+    for msg in UDPStream(settings.UDP_BIND, settings.UDP_PORT):
         decoded_message = msg.decode()
 
         if not checker.is_duplicate(decoded_message.nmea.raw):
